f8972d62cb75375a86c454215754453d193454ec
Jaizon Carlos <jaizoncarlos@proton.me>
2023-10-14 14:22:11 -0300

    Adicionou comentários sobre as funcionalidades

--- yd-modules/download.sh
+++ yd-modules/download.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-audio="-x --audio-format mp3"
-video="-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best""
-
 URL=$1
 OPTION=$2
 
+#  seleciona o formato correto para download, dependendo da escolha do usuário
 if [ $OPTION == 'video' ] ; then
-    FORMATO=$video
+    FORMATO="-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best""
 elif [ $OPTION == 'áudio' ] ; then
-    FORMATO=$audio
+    FORMATO="-x --audio-format mp3"
 else
   echo "algo deu erado"
   exit 1
@@ -19,6 +16,11 @@ clear
 echo "Agora escolha o tipo de download de $OPTION:"
 echo
 
+# selecionar se vai fazer o download do vídeo completo, dividido pelos cápitulos ou se é pra baixar uma playlist
+# o script usa o título do vpideo ou playlist para criar a pasta onde as pare irão ficar
+# no caso de capítulos, cada parte terá o nome corresponde ao nome do capítulo do vídeo
+# no caso das playlists, cada parte terá o nome correspondente do vídeo
+# em ambos os casos, as partes vêm numeradas de acordo com a ordem
 select opt in arquivo capítulos playlist sair; do
   case $opt in
     arquivo)
